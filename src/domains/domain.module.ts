4a0ac2339d7d173dd877c2471841338109183097
Quốc Chương <quocchuong2609@gmail.com>
2025-06-04 20:52:07 +0700

    add: include SemesterModule and MilestoneModule in DomainModule imports

--- src/domains/domain.module.ts
+++ src/domains/domain.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 
 import { GroupModule } from '@/groups/group.module';
 import { MajorModule } from '@/majors/major.module';
+import { MilestoneModule } from '@/milestones/milestone.module';
+import { SemesterModule } from '@/semesters/semester.module';
 import { ThesisModule } from '@/theses/thesis.module';
 
 @Module({
-	imports: [ThesisModule, MajorModule, GroupModule],
+	imports: [
+		ThesisModule,
+		MajorModule,
+		GroupModule,
+		SemesterModule,
+		MilestoneModule,
+	],
 })
 export class DomainModule {}
