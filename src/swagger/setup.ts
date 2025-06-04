4ce0a4d7342bf9e82fedc57d0869b13697f6ce9d
Minh Vương <hardingadonis@gmail.com>
2025-06-04 13:30:25 +0700

    fix: correct version type assertion in setupSwagger function

--- src/swagger/setup.ts
+++ src/swagger/setup.ts
@@ -1,13 +1,14 @@
 import { INestApplication } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-import { version } from '~/package.json';
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const { version } = require('../../package.json');
 
 export const setupSwagger = (app: INestApplication<any>) => {
 	const config = new DocumentBuilder()
 		.setTitle('TheSync Backend API doumentation')
 		.setDescription('Backend for TheSync')
-		.setVersion(version)
+		.setVersion(version as string)
 		.build();
 
 	const document = SwaggerModule.createDocument(app, config);
