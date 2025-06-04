cd184e96e002806aa6e6a0fab33bc12c9e475629
Quốc Chương <quocchuong2609@gmail.com>
2025-06-04 14:48:21 +0700

    update: add GroupModule to DomainModule imports

--- src/domains/domain.module.ts
+++ src/domains/domain.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 
+import { GroupModule } from '@/groups/group.module';
 import { MajorModule } from '@/majors/major.module';
 import { ThesisModule } from '@/theses/thesis.module';
 
 @Module({
-	imports: [ThesisModule, MajorModule],
+	imports: [ThesisModule, MajorModule, GroupModule],
 })
 export class DomainModule {}
