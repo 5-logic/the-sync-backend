995059e59ec8be5060a33cd8ddbb6a372f0bdf94
sidde1512 <binhbobinhbo22@gmail.com>
2025-06-06 08:38:19 +0700

    feat: update DomainModule imports to include Admin, Lecturer, and Student modules

--- src/domains/domain.module.ts
+++ src/domains/domain.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 
+import { AdminModule } from '@/admins/admin.module';
+import { LecturerModule } from '@/lecturers/lecturer.module';
 import { MajorModule } from '@/majors/major.module';
+import { StudentModule } from '@/students/student.module';
 import { ThesisModule } from '@/theses/thesis.module';
+import { UserModule } from '@/users/user.module';
 
 @Module({
-	imports: [ThesisModule, MajorModule],
+	imports: [
+		ThesisModule,
+		MajorModule,
+		AdminModule,
+		UserModule,
+		LecturerModule,
+		StudentModule,
+	],
 })
 export class DomainModule {}
