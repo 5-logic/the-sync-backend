1909b53f7170426570207f664ae6751937d02282
sidde1512 <binhbobinhbo22@gmail.com>
2025-06-06 08:42:30 +0700

    feat: implement Lecturer module with controller and service for managing lecturers

---
+++ src/domains/lecturers/lecturer.controller.ts
@@ -0,0 +1,48 @@
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	Param,
+	Post,
+	Put,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+
+import { CreateLecturerDto } from '@/lecturers/dto/create-lecturer.dto';
+import { UpdateLecturerDto } from '@/lecturers/dto/update-lecturer.dto';
+import { LecturerService } from '@/lecturers/lecturer.service';
+
+@ApiTags('Lecturer')
+@Controller('lecturers')
+export class LecturerController {
+	constructor(private readonly lecturerService: LecturerService) {}
+
+	@Post()
+	async create(@Body() createLecturerDto: CreateLecturerDto) {
+		return await this.lecturerService.create(createLecturerDto);
+	}
+
+	@Get()
+	async findAll() {
+		return await this.lecturerService.findAll();
+	}
+
+	@Get(':id')
+	async findOne(@Param('id') id: string) {
+		return await this.lecturerService.findOne(id);
+	}
+
+	@Put(':id')
+	async update(
+		@Param('id') id: string,
+		@Body() updateLecturerDto: UpdateLecturerDto,
+	) {
+		return await this.lecturerService.update(id, updateLecturerDto);
+	}
+
+	@Delete(':id')
+	async remove(@Param('id') id: string) {
+		return await this.lecturerService.remove(id);
+	}
+}
---
+++ src/domains/lecturers/lecturer.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+
+import { LecturerController } from '@/domains/lecturers/lecturer.controller';
+import { LecturerService } from '@/domains/lecturers/lecturer.service';
+import { PrismaService } from '@/providers/prisma.service';
+import { UserService } from '@/users/user.service';
+
+@Module({
+	controllers: [LecturerController],
+	providers: [LecturerService, PrismaService, UserService],
+})
+export class LecturerModule {}
