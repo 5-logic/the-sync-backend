23270402ed99a9cef60746027fecad3d0ec26e5d
Minh Vương <hardingadonis@gmail.com>
2025-06-13 15:55:06 +0700

    refactor: clean up import statements in admin controller

--- src/domains/admins/admin.controller.ts
+++ src/domains/admins/admin.controller.ts
@@ -1,12 +1,4 @@
-import {
-	Body,
-	Controller,
-	Delete,
-	Get,
-	Param,
-	Post,
-	Put,
-} from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
 import { AdminService } from '@/admins/admin.service';
