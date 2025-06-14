c6735c57f360e5fdb83fb0065383bfc14c46debb
sidde1512 <binhbobinhbo22@gmail.com>
2025-06-14 20:28:29 +0700

    refactor: remove milestones model

--- src/domains/milestones/dto/update-milestone.dto.ts
+++
@@ -1,5 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-
-import { CreateMilestoneDto } from '@/milestones/dto/create-milestone.dto';
-
-export class UpdateMilestoneDto extends PartialType(CreateMilestoneDto) {}
