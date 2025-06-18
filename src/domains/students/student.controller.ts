a068dca08cef96a53098030a091b45a92241d89d
Minh Vương <hardingadonis@gmail.com>
2025-06-18 14:47:20 +0700

    feat: add role-based guards to admin, lecturer, semester, and student controllers

--- src/domains/students/student.controller.ts
+++ src/domains/students/student.controller.ts
@@ -1,36 +1,56 @@
-import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
-import { ApiBody, ApiTags } from '@nestjs/swagger';
+import {
+	Body,
+	Controller,
+	Get,
+	Param,
+	Post,
+	Put,
+	UseGuards,
+} from '@nestjs/common';
+import { ApiBearerAuth, ApiBody, ApiTags } from '@nestjs/swagger';
 
+import { Roles } from '@/auth/decorators/roles.decorator';
+import { Role } from '@/auth/enums/role.enum';
+import { JwtAccessAuthGuard } from '@/auth/guards/jwt-access.guard';
+import { RoleGuard } from '@/auth/guards/role.guard';
 import { CreateStudentDto } from '@/students/dto/create-student.dto';
 import { UpdateStudentDto } from '@/students/dto/update-student.dto';
 import { StudentService } from '@/students/student.service';
 
+@UseGuards(RoleGuard)
+@UseGuards(JwtAccessAuthGuard)
+@ApiBearerAuth()
 @ApiTags('Student')
 @Controller('students')
 export class StudentController {
 	constructor(private readonly studentService: StudentService) {}
 
+	@Roles(Role.ADMIN)
 	@Post()
 	async create(@Body() createStudentDto: CreateStudentDto) {
 		return await this.studentService.create(createStudentDto);
 	}
 
+	@Roles(Role.ADMIN)
 	@ApiBody({ type: [CreateStudentDto] })
 	@Post('import')
 	async createMany(@Body() createStudentDtos: CreateStudentDto[]) {
 		return await this.studentService.createMany(createStudentDtos);
 	}
 
+	@Roles(Role.ADMIN, Role.LECTURER, Role.MODERATOR, Role.STUDENT)
 	@Get()
 	async findAll() {
 		return await this.studentService.findAll();
 	}
 
+	@Roles(Role.ADMIN, Role.LECTURER, Role.MODERATOR, Role.STUDENT)
 	@Get(':id')
 	async findOne(@Param('id') id: string) {
 		return await this.studentService.findOne(id);
 	}
 
+	@Roles(Role.ADMIN, Role.STUDENT)
 	@Put(':id')
 	async update(
 		@Param('id') id: string,
