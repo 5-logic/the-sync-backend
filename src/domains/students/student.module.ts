8dc0edb36e4fd936f117dd092d47aafc94d64298
Minh Vương <hardingadonis@gmail.com>
2025-06-27 01:45:25 +0700

    feat: add EmailModule import to Lecturer and Student modules

--- src/domains/students/student.module.ts
+++ src/domains/students/student.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 
 import { PrismaService } from '@/providers/prisma/prisma.service';
+import { EmailModule } from '@/queue/email/email.module';
 import { StudentController } from '@/students/student.controller';
 import { StudentService } from '@/students/student.service';
 
 @Module({
+	imports: [EmailModule],
 	controllers: [StudentController],
 	providers: [StudentService, PrismaService],
 })
