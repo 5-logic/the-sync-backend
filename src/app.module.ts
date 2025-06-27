82abfbba3842e9de5d5e30130bcf68c5ec23d5b5
Minh Vương <hardingadonis@gmail.com>
2025-06-27 19:44:44 +0700

    refactor: uncomment module imports in AppModule for better clarity

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,52 +19,52 @@ import { PrismaModule } from '@/providers/prisma/prisma.module';
 import { QueueModule } from '@/queue/queue.module';
 
 @Module({
-	// imports: [
-	// 	BullModule.forRootAsync({
-	// 		inject: [ConfigService],
-	// 		useFactory: (configService: ConfigService) => {
-	// 			const config = configService.get<RedisConfig>(CONFIG_TOKENS.REDIS);
-	// 			if (!config?.url) {
-	// 				throw new Error(
-	// 					'Redis configuration is not set. Please check your environment variables or configuration files.',
-	// 				);
-	// 			}
-	// 			return {
-	// 				connection: {
-	// 					url: config.url,
-	// 				},
-	// 			};
-	// 		},
-	// 	}),
-	// 	ConfigModule.forRoot({
-	// 		load: [corsConfig, redisConfig],
-	// 		cache: true,
-	// 		isGlobal: true,
-	// 	}),
-	// 	PrismaModule,
-	// 	AuthModule,
-	// 	DomainModule,
-	// 	QueueModule,
-	// 	BullBoardModule.forRootAsync({
-	// 		inject: [ConfigService],
-	// 		useFactory: (configService: ConfigService) => {
-	// 			const config = configService.get<RedisConfig>(CONFIG_TOKENS.REDIS);
-	// 			if (!config?.bullmq?.username || !config?.bullmq?.password) {
-	// 				throw new Error(
-	// 					'BullMQ configuration is not set. Please check your environment variables or configuration files.',
-	// 				);
-	// 			}
-	// 			return {
-	// 				route: `/${CONFIG_MOUNTS.BULL_BOARD}`,
-	// 				adapter: ExpressAdapter,
-	// 				middleware: basicAuth({
-	// 					challenge: true,
-	// 					users: { [config.bullmq.username]: config.bullmq.password },
-	// 				}),
-	// 			};
-	// 		},
-	// 	}),
-	// ],
+	imports: [
+		// BullModule.forRootAsync({
+		// 	inject: [ConfigService],
+		// 	useFactory: (configService: ConfigService) => {
+		// 		const config = configService.get<RedisConfig>(CONFIG_TOKENS.REDIS);
+		// 		if (!config?.url) {
+		// 			throw new Error(
+		// 				'Redis configuration is not set. Please check your environment variables or configuration files.',
+		// 			);
+		// 		}
+		// 		return {
+		// 			connection: {
+		// 				url: config.url,
+		// 			},
+		// 		};
+		// 	},
+		// }),
+		// ConfigModule.forRoot({
+		// 	load: [corsConfig, redisConfig],
+		// 	cache: true,
+		// 	isGlobal: true,
+		// }),
+		PrismaModule,
+		AuthModule,
+		// DomainModule,
+		// QueueModule,
+		// BullBoardModule.forRootAsync({
+		// 	inject: [ConfigService],
+		// 	useFactory: (configService: ConfigService) => {
+		// 		const config = configService.get<RedisConfig>(CONFIG_TOKENS.REDIS);
+		// 		if (!config?.bullmq?.username || !config?.bullmq?.password) {
+		// 			throw new Error(
+		// 				'BullMQ configuration is not set. Please check your environment variables or configuration files.',
+		// 			);
+		// 		}
+		// 		return {
+		// 			route: `/${CONFIG_MOUNTS.BULL_BOARD}`,
+		// 			adapter: ExpressAdapter,
+		// 			middleware: basicAuth({
+		// 				challenge: true,
+		// 				users: { [config.bullmq.username]: config.bullmq.password },
+		// 			}),
+		// 		};
+		// 	},
+		// }),
+	],
 })
 export class AppModule {
 	// configure(consumer: MiddlewareConsumer) {
