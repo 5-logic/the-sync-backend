09d876105e512f4ef636f13ff5e1b90d763f4687
Quốc Chương <quocchuong2609@gmail.com>
2025-06-30 15:45:28 +0700

    feat: add UpdateUserPasswordDto with validation for current and new passwords

--- src/domains/users/dto/update-user.dto.ts
+++ src/domains/users/dto/update-user.dto.ts
@@ -1,7 +1,24 @@
-import { OmitType, PartialType } from '@nestjs/swagger';
+import { ApiProperty, OmitType, PartialType } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, Matches, MinLength } from 'class-validator';
 
 import { CreateUserDto } from '@/domains/users/dto/create-user.dto';
 
 export class UpdateUserDto extends PartialType(
 	OmitType(CreateUserDto, ['email'] as const),
 ) {}
+
+export class UpdateUserPasswordDto {
+	@ApiProperty()
+	@IsString()
+	@IsNotEmpty()
+	@MinLength(12)
+	@Matches(/^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).+$/)
+	currentPassword: string;
+
+	@ApiProperty()
+	@IsString()
+	@IsNotEmpty()
+	@MinLength(12)
+	@Matches(/^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).+$/)
+	newPassword: string;
+}
