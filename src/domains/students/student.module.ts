3aa284e729ee611e4aca41b0ecb8cd349591d838
Quốc Chương <quocchuong2609@gmail.com>
2025-06-30 15:47:23 +0700

    feat: add changePassword method to StudentController and include UserService in StudentModule

--- src/domains/students/student.module.ts
+++ src/domains/students/student.module.ts
@@ -3,10 +3,11 @@ import { Module } from '@nestjs/common';
 import { EmailModule } from '@/queue/email/email.module';
 import { StudentController } from '@/students/student.controller';
 import { StudentService } from '@/students/student.service';
+import { UserService } from '@/users/user.service';
 
 @Module({
 	imports: [EmailModule],
 	controllers: [StudentController],
-	providers: [StudentService],
+	providers: [StudentService, UserService],
 })
 export class StudentModule {}
