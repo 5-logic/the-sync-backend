4b0beb70f154198668be34286543dc08d7702673
Quốc Chương <quocchuong2609@gmail.com>
2025-07-01 14:12:12 +0700

    feat: add EmailModule import to ThesisModule

--- src/domains/theses/thesis.module.ts
+++ src/domains/theses/thesis.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 
+import { EmailModule } from '@/queue/email/email.module';
 import { ThesisController } from '@/theses/thesis.controller';
 import { ThesisService } from '@/theses/thesis.service';
 
 @Module({
+	imports: [EmailModule],
 	controllers: [ThesisController],
 	providers: [ThesisService],
 })
