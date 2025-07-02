fc7b6c88e6adb16f67e4a47ce848964d7c9194c8
Minh Vương <hardingadonis@gmail.com>
2025-07-02 14:40:21 +0700

    feat: integrate caching with Keyv and CacheableMemory in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,23 @@
 import { ExpressAdapter } from '@bull-board/express';
 import { BullBoardModule } from '@bull-board/nestjs';
+import { createKeyv } from '@keyv/redis';
 import { BullModule } from '@nestjs/bullmq';
+import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import basicAuth from 'basic-auth-connect';
+import { CacheableMemory } from 'cacheable';
+import { Keyv } from 'keyv';
 
 import { AuthModule } from '@/auth/auth.module';
 import {
 	CONFIG_MOUNTS,
 	CONFIG_TOKENS,
+	CONSTANTS,
 	RedisConfig,
+	corsConfig,
 	redisConfig,
 } from '@/configs';
-import { corsConfig } from '@/configs/cors.config';
 import { DomainModule } from '@/domains/domain.module';
 import { PrismaModule } from '@/providers/prisma/prisma.module';
 import { QueueModule } from '@/queue/queue.module';
@@ -36,6 +41,31 @@ import { QueueModule } from '@/queue/queue.module';
 					},
 				};
 			},
+		}),
+		CacheModule.registerAsync({
+			inject: [ConfigService],
+			useFactory: (configService: ConfigService) => {
+				const config = configService.get<RedisConfig>(CONFIG_TOKENS.REDIS);
+
+				if (!config?.url) {
+					throw new Error(
+						'Redis configuration is not set. Please check your environment variables or configuration files.',
+					);
+				}
+
+				return {
+					stores: [
+						new Keyv({
+							store: new CacheableMemory({
+								ttl: CONSTANTS.TTL,
+								lruSize: CONSTANTS.LRU_SIZE,
+							}),
+						}),
+						createKeyv(config.url),
+					],
+				};
+			},
+			isGlobal: true,
 		}),
 		ConfigModule.forRoot({
 			load: [corsConfig, redisConfig],
