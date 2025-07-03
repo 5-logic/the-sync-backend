9d8522c1f242078f230c1e608be1af7d137ece3e
Quốc Chương <quocchuong2609@gmail.com>
2025-07-03 16:13:27 +0700

    fix: remove 'code' field from UpdateGroupDto OmitType

--- src/domains/groups/dto/update-group.dto.ts
+++ src/domains/groups/dto/update-group.dto.ts
@@ -3,5 +3,5 @@ import { OmitType, PartialType } from '@nestjs/swagger';
 import { CreateGroupDto } from '@/groups/dto/create-group.dto';
 
 export class UpdateGroupDto extends PartialType(
-	OmitType(CreateGroupDto, ['code', 'semesterId'] as const),
+	OmitType(CreateGroupDto, ['semesterId'] as const),
 ) {}
