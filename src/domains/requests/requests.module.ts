70c74362048b7fb7ca82f5f9d6237134a50ba4e6
Quốc Chương <quocchuong2609@gmail.com>
2025-07-03 21:33:08 +0700

    feat: add EmailModule import to RequestsModule

--- src/domains/requests/requests.module.ts
+++ src/domains/requests/requests.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 
+import { EmailModule } from '@/queue/email/email.module';
 import { RequestsController } from '@/requests/requests.controller';
 import { RequestsService } from '@/requests/requests.service';
 
 @Module({
+	imports: [EmailModule],
 	controllers: [RequestsController],
 	providers: [RequestsService],
 })
