14c926faf4e653e85bb37beeee34fddd49fe373f
Quốc Chương <quocchuong2609@gmail.com>
2025-07-03 21:22:29 +0700

    feat: add new email job types for join and invite request notifications

--- src/queue/email/enums/type.enum.ts
+++ src/queue/email/enums/type.enum.ts
@@ -5,4 +5,7 @@ export enum EmailJobType {
 	SEND_SUPERVISION_NOTIFICATION = 'send-supervision-notification',
 	SEND_OTP = 'send-otp',
 	SEND_NOTIFICATION = 'send-notification',
+	SEND_JOIN_REQUEST_NOTIFICATION = 'send-join-request-notification',
+	SEND_INVITE_REQUEST_NOTIFICATION = 'send-invite-request-notification',
+	SEND_REQUEST_STATUS_UPDATE = 'send-request-status-update',
 }
