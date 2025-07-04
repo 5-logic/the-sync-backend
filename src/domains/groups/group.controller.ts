34ca8d1748bcbd118a6d179c512f7cdb455db9ee
Quốc Chương <quocchuong2609@gmail.com>
2025-07-04 11:44:46 +0700

    feat: add changeLeader method to GroupController for updating group leader

--- src/domains/groups/group.controller.ts
+++ src/domains/groups/group.controller.ts
@@ -13,7 +13,7 @@ import { Request } from 'express';
 
 import { JwtAccessAuthGuard, Role, RoleGuard, Roles } from '@/auth';
 import { UserPayload } from '@/auth/interfaces/user-payload.interface';
-import { CreateGroupDto, UpdateGroupDto } from '@/groups/dto';
+import { ChangeLeaderDto, CreateGroupDto, UpdateGroupDto } from '@/groups/dto';
 import { GroupService } from '@/groups/group.service';
 
 @UseGuards(JwtAccessAuthGuard, RoleGuard)
@@ -36,6 +36,18 @@ export class GroupController {
 		return await this.groupService.findAll();
 	}
 
+	@Get('student')
+	async findMyGroups(@Req() req: Request) {
+		const user = req.user as UserPayload;
+
+		return await this.groupService.findByStudentId(user.id);
+	}
+
+	@Get('student/:studentId')
+	async findByStudentId(@Param('studentId') studentId: string) {
+		return await this.groupService.findByStudentId(studentId);
+	}
+
 	@Get(':id')
 	async findOne(@Param('id') id: string) {
 		return await this.groupService.findOne(id);
@@ -51,5 +63,27 @@ export class GroupController {
 		const user = req.user as UserPayload;
 
 		return await this.groupService.update(id, user.id, dto);
+	}
+
+	@Roles(Role.STUDENT)
+	@Put(':id/change-leader')
+	async changeLeader(
+		@Req() req: Request,
+		@Param('id') id: string,
+		@Body() dto: ChangeLeaderDto,
+	) {
+		const user = req.user as UserPayload;
+
+		return await this.groupService.changeLeader(id, user.id, dto);
+	}
+
+	@Get(':id/members')
+	async findGroupMembers(@Param('id') id: string) {
+		return await this.groupService.findGroupMembers(id);
+	}
+
+	@Get(':id/skills-responsibilities')
+	async findGroupSkillsAndResponsibilities(@Param('id') id: string) {
+		return await this.groupService.findGroupSkillsAndResponsibilities(id);
 	}
 }
