495d035289dde7fe0fa332b8e5d356b65aabeb6f
Quốc Chương <quocchuong2609@gmail.com>
2025-07-06 12:27:51 +0700

    feat: export AssignThesisDto in index file for thesis management

--- src/domains/theses/dto/index.ts
+++ src/domains/theses/dto/index.ts
@@ -2,3 +2,4 @@ export * from '@/theses/dto/create-thesis.dto';
 export * from '@/theses/dto/review-thesis.dto';
 export * from '@/theses/dto/update-thesis.dto';
 export * from '@/theses/dto/publish-theses.dto';
+export * from '@/theses/dto/assign-thesis.dto';
