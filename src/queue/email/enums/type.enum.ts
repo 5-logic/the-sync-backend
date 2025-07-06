91dd23d80f6a27b6a80a76364db2f3e880c6aed0
Quốc Chương <quocchuong2609@gmail.com>
2025-07-06 11:57:25 +0700

    feat: add SEND_GROUP_MEMBER_CHANGE_NOTIFICATION to EmailJobType enum

--- src/queue/email/enums/type.enum.ts
+++ src/queue/email/enums/type.enum.ts
@@ -1,5 +1,6 @@
 export enum EmailJobType {
 	SEND_GROUP_LEADER_CHANGE_NOTIFICATION = 'send-group-leader-change-notification',
+	SEND_GROUP_MEMBER_CHANGE_NOTIFICATION = 'send-group-member-change-notification',
 	SEND_INVITE_REQUEST_NOTIFICATION = 'send-invite-request-notification',
 	SEND_JOIN_REQUEST_NOTIFICATION = 'send-join-request-notification',
 	SEND_LECTURER_ACCOUNT = 'send-lecturer-account',
