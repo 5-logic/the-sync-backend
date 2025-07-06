638285b63215a9b49ea389ed94f94c3fe82ce15b
Quốc Chương <quocchuong2609@gmail.com>
2025-07-06 11:56:55 +0700

    feat: export AssignStudentDto and RemoveStudentDto in groups DTO index

--- src/domains/groups/dto/index.ts
+++ src/domains/groups/dto/index.ts
@@ -1,3 +1,5 @@
 export * from '@/groups/dto/create-group.dto';
 export * from '@/groups/dto/update-group.dto';
 export * from '@/groups/dto/change-leader.dto';
+export * from '@/groups/dto/assign-student.dto';
+export * from '@/groups/dto/remove-student.dto';
