5c2fcab82e3f195c399b1d811f2fa5bc0b1fcfa7
Quốc Chương <quocchuong2609@gmail.com>
2025-07-07 10:45:29 +0700

    feat: integrate GroupService into ThesisModule and ThesisService for email notifications

--- src/domains/theses/thesis.module.ts
+++ src/domains/theses/thesis.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 
+import { GroupModule } from '@/groups/group.module';
 import { EmailModule } from '@/queue/email/email.module';
 import { ThesisController } from '@/theses/thesis.controller';
 import { ThesisService } from '@/theses/thesis.service';
 
 @Module({
-	imports: [EmailModule],
+	imports: [EmailModule, GroupModule],
 	controllers: [ThesisController],
 	providers: [ThesisService],
 })
