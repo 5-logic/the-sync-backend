98d0a590b51f0401dd29f2e5e09996e0ff0c773e
Quốc Chương <quocchuong2609@gmail.com>
2025-07-07 10:39:08 +0700

    feat: export PickThesisDto from groups DTO index

--- src/domains/groups/dto/index.ts
+++ src/domains/groups/dto/index.ts
@@ -3,3 +3,4 @@ export * from '@/groups/dto/update-group.dto';
 export * from '@/groups/dto/change-leader.dto';
 export * from '@/groups/dto/assign-student.dto';
 export * from '@/groups/dto/remove-student.dto';
+export * from '@/groups/dto/pick-thesis.dto';
