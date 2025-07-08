54b4af560768cf0321e6a40388fdfe0148db2aed
Quốc Chương <quocchuong2609@gmail.com>
2025-07-08 20:19:53 +0700

    feat: implement CreateChecklistItemDto with validation and Swagger properties

--- src/domains/checklists/checklist-items/dto/create-checklist-item.dto.ts
+++ src/domains/checklists/checklist-items/dto/create-checklist-item.dto.ts
@@ -1,1 +1,35 @@
-export class CreateChecklistItemDto {}
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import {
+	IsBoolean,
+	IsEnum,
+	IsOptional,
+	IsString,
+	IsUUID,
+} from 'class-validator';
+
+import { ChecklistReviewAcceptance } from '~/generated/prisma';
+
+export class CreateChecklistItemDto {
+	@ApiProperty()
+	@IsString()
+	name: string;
+
+	@ApiPropertyOptional()
+	@IsOptional()
+	@IsString()
+	description?: string;
+
+	@ApiPropertyOptional()
+	@IsOptional()
+	@IsEnum(ChecklistReviewAcceptance)
+	acceptance?: ChecklistReviewAcceptance;
+
+	@ApiPropertyOptional()
+	@IsOptional()
+	@IsBoolean()
+	isRequired?: boolean;
+
+	@ApiProperty()
+	@IsUUID()
+	checklistId: string;
+}
