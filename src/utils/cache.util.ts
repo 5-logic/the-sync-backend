2a51d43a52b0b5083ff224ad9578e74c6c49bfad
Minh Vương <hardingadonis@gmail.com>
2025-07-12 17:18:47 +0700

    feat: implement utility functions for Redis configuration and cache store creation

---
+++ src/utils/cache.util.ts
@@ -0,0 +1,16 @@
+import { createKeyv } from '@keyv/redis';
+import { CacheableMemory } from 'cacheable';
+import { Keyv } from 'keyv';
+
+import { CONSTANTS } from '@/configs';
+import type { RedisConfig } from '@/configs';
+
+export const createCacheStores = (config: RedisConfig) => [
+	new Keyv({
+		store: new CacheableMemory({
+			ttl: CONSTANTS.TTL,
+			lruSize: CONSTANTS.LRU_SIZE,
+		}),
+	}),
+	createKeyv(config.url),
+];
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,4 @@
+export * from '@/utils/cache.util';
 export * from '@/utils/generator.util';
 export * from '@/utils/hash.util';
+export * from '@/utils/redis.util';
