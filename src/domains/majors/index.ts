103a44363ab2caa0d2788054a3400de6711b9fd0
Minh Vương <hardingadonis@gmail.com>
2025-07-16 00:07:45 +0700

    feat: implement major module with controller, service, constants, and documentation

---
+++ src/domains/majors/index.ts
@@ -0,0 +1,5 @@
+export * from '@/majors/constants';
+export * from '@/majors/controllers';
+export * from '@/majors/docs';
+export * from '@/majors/services';
+export * from '@/majors/major.module';
