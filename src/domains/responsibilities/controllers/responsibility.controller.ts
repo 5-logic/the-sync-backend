638f663eeb10cbfb033ffa083826448d1933f019
Minh Vương <hardingadonis@gmail.com>
2025-07-16 15:19:25 +0700

    feat: enhance response types and caching improvements

--- src/domains/responsibilities/controllers/responsibility.controller.ts
+++ src/domains/responsibilities/controllers/responsibility.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import {
+	Controller,
+	Get,
+	HttpCode,
+	HttpStatus,
+	Param,
+	UseGuards,
+} from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 
 import { JwtAccessAuthGuard, RoleGuard } from '@/auth';
+import { ApiArrayResponse, ApiBaseResponse } from '@/common';
 import {
 	RESPONSIBILITY_API_TAGS,
 	RESPONSIBILITY_CONSTANTS,
 } from '@/responsibilities/constants';
 import { ResponsibilityDocs } from '@/responsibilities/docs';
+import { ResponsibilityResponse } from '@/responsibilities/responses';
 import { ResponsibilityService } from '@/responsibilities/services';
 
 @UseGuards(JwtAccessAuthGuard, RoleGuard)
@@ -16,15 +25,19 @@ import { ResponsibilityService } from '@/responsibilities/services';
 export class ResponsibilityController {
 	constructor(private readonly service: ResponsibilityService) {}
 
+	@HttpCode(HttpStatus.OK)
 	@Get()
 	@ApiOperation(ResponsibilityDocs.findAll)
-	async findAll() {
+	@ApiArrayResponse(ResponsibilityResponse, HttpStatus.OK)
+	async findAll(): Promise<ResponsibilityResponse[]> {
 		return await this.service.findAll();
 	}
 
+	@HttpCode(HttpStatus.OK)
 	@Get(':id')
 	@ApiOperation(ResponsibilityDocs.findOne)
-	async findOne(@Param('id') id: string) {
+	@ApiBaseResponse(ResponsibilityResponse, HttpStatus.OK)
+	async findOne(@Param('id') id: string): Promise<ResponsibilityResponse> {
 		return await this.service.findOne(id);
 	}
 }
