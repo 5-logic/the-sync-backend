305a27d3203fc248c54fead5a54dcbaf3a9ed01f
Minh Vương <hardingadonis@gmail.com>
2025-07-19 15:38:24 +0700

    fix: update import path for CreateUserDto in UpdateLecturerDto

--- src/domains/lecturers/dto/update-lecturer.dto.ts
+++ src/domains/lecturers/dto/update-lecturer.dto.ts
@@ -1,5 +1,5 @@
 import { PartialType } from '@nestjs/swagger';
 
-import { CreateUserDto } from '@/users/dto/create-user.dto';
+import { CreateUserDto } from '@/users/index';
 
 export class UpdateLecturerDto extends PartialType(CreateUserDto) {}
