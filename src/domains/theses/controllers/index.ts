0a29348a4f138b429c99ef3d9c95df95598b4760
Minh Vương <hardingadonis@gmail.com>
2025-07-20 22:32:27 +0700

    refactor: replace ThesisController with ThesisLecturerController and update module exports

--- src/domains/theses/controllers/index.ts
+++ src/domains/theses/controllers/index.ts
@@ -1,3 +1,3 @@
-export * from '@/theses/controllers/thesis-publish.controller';
+export * from '@/theses/controllers/thesis-lecturer.controller';
 export * from '@/theses/controllers/thesis-moderator.controller';
 export * from '@/theses/controllers/thesis-publish.controller';
--- src/domains/theses/thesis.module.ts
+++ src/domains/theses/thesis.module.ts
@@ -1,29 +1,26 @@
 import { Module } from '@nestjs/common';
 
-import { GroupModule } from '@/groups/group.module';
-import { EmailModule } from '@/queue/email/email.module';
-import { ThesisLecturerController } from '@/theses/controllers/thesis-lecturer.controller';
-import { ThesisModeratorController } from '@/theses/controllers/thesis-moderator.controller';
-import { ThesisPublishController } from '@/theses/controllers/thesis-publish.controller';
-import { ThesisLecturerService } from '@/theses/services/thesis-lecturer.service';
-import { ThesisModeratorService } from '@/theses/services/thesis-moderator.service';
-import { ThesisPublishService } from '@/theses/services/thesis-publish.service';
-import { ThesisController } from '@/theses/thesis.controller';
-import { ThesisService } from '@/theses/thesis.service';
+import {
+	ThesisLecturerController,
+	ThesisModeratorController,
+	ThesisPublishController,
+} from '@/theses/controllers';
+import {
+	ThesisLecturerService,
+	ThesisModeratorService,
+	ThesisPublishService,
+} from '@/theses/services';
 
 @Module({
-	imports: [EmailModule, GroupModule],
 	controllers: [
-		ThesisController,
-		// ThesisPublishController,
-		// ThesisLecturerController,
-		// ThesisModeratorController,
+		ThesisLecturerController,
+		ThesisModeratorController,
+		ThesisPublishController,
 	],
 	providers: [
-		ThesisService,
-		// ThesisPublishService,
-		// ThesisLecturerService,
-		// ThesisModeratorService,
+		ThesisLecturerService,
+		ThesisModeratorService,
+		ThesisPublishService,
 	],
 })
 export class ThesisModule {}
