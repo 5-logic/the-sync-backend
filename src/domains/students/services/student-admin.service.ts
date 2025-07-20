870e8afea73df15b013a8192b5d76bf81cd5849c
Minh Vương <hardingadonis@gmail.com>
2025-07-20 15:47:17 +0700

    feat: implement toggle student status functionality with error handling and logging

--- src/domains/students/services/student-admin.service.ts
+++ src/domains/students/services/student-admin.service.ts
@@ -1,5 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 
 import { PrismaService } from '@/providers';
 import {
@@ -8,6 +7,8 @@ import {
 	ToggleStudentStatusDto,
 	UpdateStudentDto,
 } from '@/students/dtos';
+import { mapStudentV1 } from '@/students/mappers';
+import { StudentResponse } from '@/students/responses';
 
 @Injectable()
 export class StudentAdminService {
@@ -28,7 +29,48 @@ export class StudentAdminService {
 	}
 
 	async toggleStatus(id: string, dto: ToggleStudentStatusDto) {
-		await new Promise((resolve) => setTimeout(resolve, 10));
+		this.logger.log(
+			`Toggling status for student with ID: ${id}, isActive: ${dto.isActive}`,
+		);
+
+		try {
+			const result = await this.prisma.$transaction(async (prisma) => {
+				const existingStudent = await prisma.student.findUnique({
+					where: { userId: id },
+				});
+
+				if (!existingStudent) {
+					this.logger.warn(`Student with ID ${id} not found for status toggle`);
+
+					throw new NotFoundException(`Student not found`);
+				}
+
+				const updatedUser = await prisma.user.update({
+					where: { id },
+					data: {
+						isActive: dto.isActive,
+					},
+				});
+
+				const result: StudentResponse = mapStudentV1(
+					updatedUser,
+					existingStudent,
+				);
+
+				return result;
+			});
+
+			this.logger.log(
+				`Student status updated - ID: ${id}, isActive: ${dto.isActive}`,
+			);
+			this.logger.debug('Updated student status', JSON.stringify(result));
+
+			return result;
+		} catch (error) {
+			this.logger.error(`Error toggling student status with ID ${id}`, error);
+
+			throw error;
+		}
 	}
 
 	async delete(id: string, semesterId: string) {
