60666e631353e3197a7da28f00ed6d427dfd21f4
Minh Vương <hardingadonis@gmail.com>
2025-07-20 16:15:20 +0700

    feat: restructure student module by removing deprecated controller and service, and consolidating exports

--- src/domains/students/student.module.ts
+++ src/domains/students/student.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
 
-import { EmailModule } from '@/queue/email/email.module';
-import { StudentController } from '@/students/student.controller';
-import { StudentService } from '@/students/student.service';
+import {
+	StudentAdminController,
+	StudentPublicController,
+	StudentSelfController,
+} from '@/students/controllers';
+import {
+	StudentAdminService,
+	StudentPublicService,
+	StudentSelfService,
+} from '@/students/services';
 
 @Module({
-	imports: [EmailModule],
-	controllers: [StudentController],
-	providers: [StudentService],
+	controllers: [
+		StudentAdminController,
+		StudentPublicController,
+		StudentSelfController,
+	],
+	providers: [StudentAdminService, StudentPublicService, StudentSelfService],
 })
 export class StudentModule {}
