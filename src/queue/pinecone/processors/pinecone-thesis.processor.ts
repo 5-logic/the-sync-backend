508abb32ef77ec3a1813f217ffd2608e0876897b
Minh Vương <hardingadonis@gmail.com>
2025-07-21 16:58:26 +0700

    feat: enhance PineconeThesisProcessor to handle job types and improve error logging

--- src/queue/pinecone/processors/pinecone-thesis.processor.ts
+++ src/queue/pinecone/processors/pinecone-thesis.processor.ts
@@ -5,10 +5,13 @@ import { Job } from 'bullmq';
 
 import { PineconeConfig, pineconeConfig } from '@/configs';
 import { PINECONE_TOKENS } from '@/queue/pinecone/constants';
+import { PineconeJobType } from '@/queue/pinecone/enums';
 import { ThesisDetailResponse } from '@/theses/responses';
 
 @Processor(PINECONE_TOKENS.THESIS)
 export class PineconeThesisProcessor extends WorkerHost {
+	private static readonly NAMESPACE = PINECONE_TOKENS.THESIS + ':namespace';
+
 	private readonly logger = new Logger(PineconeThesisProcessor.name);
 
 	private readonly pineconeClient: Pinecone;
@@ -29,7 +32,40 @@ export class PineconeThesisProcessor extends WorkerHost {
 		);
 	}
 
-	process(job: Job<ThesisDetailResponse>): Promise<void> {
+	async process(job: Job<ThesisDetailResponse>): Promise<void> {
+		try {
+			switch (job.name as PineconeJobType) {
+				case PineconeJobType.CREATE_OR_UPDATE: {
+					await this.createOrUpdate(job.data);
+					break;
+				}
+				case PineconeJobType.DELETE: {
+					await this.delete(job.data);
+					break;
+				}
+
+				default: {
+					this.logger.warn(`Unknown job type: ${job.name}`);
+
+					throw new Error(`Unknown job type: ${job.name}`);
+				}
+			}
+		} catch (error) {
+			this.logger.error(`Failed to process job ${job.id}:`, error);
+
+			throw error;
+		}
+	}
+
+	async createOrUpdate(dto: ThesisDetailResponse): Promise<void> {
 		throw new Error('Method not implemented.');
+	}
+
+	async delete(dto: ThesisDetailResponse): Promise<void> {
+		const index = this.pineconeClient
+			.Index(this.pineconeConfiguration.indexName)
+			.namespace(PineconeThesisProcessor.NAMESPACE);
+
+		await index.deleteOne(dto.id);
 	}
 }
