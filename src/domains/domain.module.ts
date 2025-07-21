d3c2d4f0513ff857e1f275bd11ac272ab0983145
Minh Vương <hardingadonis@gmail.com>
2025-07-21 18:50:09 +0700

    refactor: update import paths in DomainModule for consistency

--- src/domains/domain.module.ts
+++ src/domains/domain.module.ts
@@ -1,21 +1,21 @@
 import { Module } from '@nestjs/common';
 
-import { AdminModule } from '@/admins/admin.module';
-import { AIModule } from '@/ai/ai.module';
+import { AdminModule } from '@/admins/index';
+import { AIModule } from '@/ai/index';
 import { ChecklistModule } from '@/checklists/checklist.module';
 import { GroupModule } from '@/groups/group.module';
-import { LecturerModule } from '@/lecturers/lecturer.module';
-import { MajorModule } from '@/majors/major.module';
+import { LecturerModule } from '@/lecturers/index';
+import { MajorModule } from '@/majors/index';
 import { MilestoneModule } from '@/milestones/milestone.module';
 import { RequestModule } from '@/requests/request.module';
 import { ResponsibilityModule } from '@/responsibilities/responsibility.module';
 import { ReviewModule } from '@/reviews/review.module';
-import { SemesterModule } from '@/semesters/semester.module';
-import { SkillSetModule } from '@/skill-sets/skill-set.module';
-import { StudentModule } from '@/students/student.module';
+import { SemesterModule } from '@/semesters/index';
+import { SkillSetModule } from '@/skill-sets/index';
+import { StudentModule } from '@/students/index';
 import { SubmissionModule } from '@/submissions/submission.module';
 import { SupervisionModule } from '@/supervisions/supervision.module';
-import { ThesisModule } from '@/theses/thesis.module';
+import { ThesisModule } from '@/theses/index';
 
 @Module({
 	imports: [
