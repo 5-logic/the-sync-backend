2d4174ed187a06bb5fffaecdcfcf9e9f53d20eee
Minh Vương <hardingadonis@gmail.com>
2025-07-21 16:39:05 +0700

    feat: update Pinecone configuration to enforce required fields and add Pinecone module with thesis processor

--- src/queue/pinecone/processors/pinecone-thesis.processor.ts
+++ src/queue/pinecone/processors/pinecone-thesis.processor.ts
@@ -1,7 +1,9 @@
 import { Processor, WorkerHost } from '@nestjs/bullmq';
-import { Logger } from '@nestjs/common';
+import { Inject, Logger } from '@nestjs/common';
+import { Pinecone } from '@pinecone-database/pinecone';
 import { Job } from 'bullmq';
 
+import { PineconeConfig, pineconeConfig } from '@/configs';
 import { PINECONE_TOKENS } from '@/queue/pinecone/constants';
 import { ThesisDetailResponse } from '@/theses/responses';
 
@@ -9,8 +11,22 @@ import { ThesisDetailResponse } from '@/theses/responses';
 export class PineconeThesisProcessor extends WorkerHost {
 	private readonly logger = new Logger(PineconeThesisProcessor.name);
 
-	constructor() {
+	private readonly pineconeClient: Pinecone;
+
+	constructor(
+		@Inject(pineconeConfig.KEY)
+		private readonly pineconeConfiguration: PineconeConfig,
+	) {
 		super();
+
+		this.pineconeClient = new Pinecone({
+			apiKey: pineconeConfiguration.apiKey,
+		});
+
+		this.logger.log('Pinecone client initialized successfully.');
+		this.logger.debug(
+			`Pinecone environment: ${JSON.stringify(pineconeConfiguration.apiKey)}`,
+		);
 	}
 
 	process(job: Job<ThesisDetailResponse>): Promise<void> {
