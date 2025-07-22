0536c5d9dbe4aadfcb441d42a37b41047ab33574
Minh Vương <hardingadonis@gmail.com>
2025-07-22 22:17:40 +0700

    refactor: streamline Redis and Pinecone configurations by removing unused utilities and integrating directly into app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,22 @@
 import { ExpressAdapter } from '@bull-board/express';
 import { BullBoardModule } from '@bull-board/nestjs';
+import { createKeyv } from '@keyv/redis';
 import { BullModule } from '@nestjs/bullmq';
 import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import basicAuth from 'basic-auth-connect';
+import { CacheableMemory } from 'cacheable';
+import Keyv from 'keyv';
 
 import { AuthModule } from '@/auth/auth.module';
-import { CONFIG_MOUNTS, corsConfig, redisConfig } from '@/configs';
+import {
+	CONFIG_MOUNTS,
+	CONSTANTS,
+	RedisConfig,
+	corsConfig,
+	redisConfig,
+} from '@/configs';
 import { DomainModule } from '@/domains/domain.module';
 import {
 	CacheHelperModule,
@@ -15,11 +24,6 @@ import {
 	PrismaModule,
 } from '@/providers';
 import { QueueModule } from '@/queue/queue.module';
-import {
-	createCacheStores,
-	getBullBoardAuthOrThrow,
-	getRedisConfigOrThrow,
-} from '@/utils';
 
 @Module({
 	imports: [
@@ -30,9 +34,8 @@ import {
 		}),
 
 		BullModule.forRootAsync({
-			inject: [ConfigService],
-			useFactory: (configService: ConfigService) => {
-				const config = getRedisConfigOrThrow(configService);
+			inject: [redisConfig.KEY],
+			useFactory: (config: RedisConfig) => {
 				return {
 					connection: {
 						url: config.url,
@@ -42,25 +45,30 @@ import {
 		}),
 
 		CacheModule.registerAsync({
-			inject: [ConfigService],
-			useFactory: (configService: ConfigService) => {
-				const config = getRedisConfigOrThrow(configService);
+			inject: [redisConfig.KEY],
+			useFactory: (config: RedisConfig) => {
 				return {
-					stores: createCacheStores(config),
+					stores: [
+						new Keyv({
+							store: new CacheableMemory({
+								ttl: CONSTANTS.TTL,
+								lruSize: CONSTANTS.LRU_SIZE,
+							}),
+						}),
+						createKeyv(config.url),
+					],
 				};
 			},
 			isGlobal: true,
 		}),
 
 		BullBoardModule.forRootAsync({
-			inject: [ConfigService],
-			useFactory: (configService: ConfigService) => {
-				const config = getRedisConfigOrThrow(configService);
-				const { username, password } = getBullBoardAuthOrThrow(config);
+			inject: [redisConfig.KEY],
+			useFactory: (config: RedisConfig) => {
 				return {
 					route: `/${CONFIG_MOUNTS.BULL_BOARD}`,
 					adapter: ExpressAdapter,
-					middleware: basicAuth(username, password),
+					middleware: basicAuth(config.bullmq.username, config.bullmq.password),
 				};
 			},
 		}),
