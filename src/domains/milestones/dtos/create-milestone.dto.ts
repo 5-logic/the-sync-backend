f965e1acb5d1f0482df9816d7531d6998922abf5
Quốc Chương <quocchuong2609@gmail.com>
2025-07-22 08:34:24 +0700

    feat: add CreateMilestoneDto for milestone creation

---
+++ src/domains/milestones/dtos/create-milestone.dto.ts
@@ -0,0 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsDateString, IsString, IsUUID } from 'class-validator';
+
+export class CreateMilestoneDto {
+	@ApiProperty()
+	@IsString()
+	name: string;
+
+	@ApiProperty()
+	@IsDateString()
+	startDate: Date;
+
+	@ApiProperty()
+	@IsDateString()
+	endDate: Date;
+
+	@ApiProperty()
+	@IsUUID()
+	semesterId: string;
+
+	@ApiProperty()
+	@IsString({ each: true })
+	documents: string[];
+}
