061b73ef2f3f011c7077bcf86041c5492437e529
Quốc Chương <quocchuong2609@gmail.com>
2025-07-22 15:29:07 +0700

    feat: remove deprecated milestone DTOs and controller/service files

--- src/domains/milestones/dto/update-milestone.dto.ts
+++
@@ -1,7 +0,0 @@
-import { OmitType, PartialType } from '@nestjs/swagger';
-
-import { CreateMilestoneDto } from '@/milestones/dto/create-milestone.dto';
-
-export class UpdateMilestoneDto extends PartialType(
-	OmitType(CreateMilestoneDto, ['semesterId'] as const),
-) {}
