661aa6838b09034e2810143805e06b82709ccab1
Quốc Chương <quocchuong2609@gmail.com>
2025-07-23 10:33:31 +0700

    feat: create index file for requests module exports

---
+++ src/domains/requests/index.ts
@@ -0,0 +1,6 @@
+export * from '@/requests/constants';
+export * from '@/requests/controllers';
+export * from '@/requests/dtos';
+export * from '@/requests/mappers';
+export * from '@/requests/responses';
+export * from '@/requests/services';
