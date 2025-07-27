c5962ac2cbeb8a7d96cd1efc9acdacf0dff93446
Quốc Chương <quocchuong2609@gmail.com>
2025-07-27 16:29:26 +0700

    feat: remove dashboard constants and related index export

--- src/domains/dashboards/constants/dashboard.constant.ts
+++
@@ -1,9 +0,0 @@
-export const DASHBOARD_API_TAGS = 'Dashboard';
-
-export const BASE_DASHBOARD_PATH = 'dashboards';
-
-export const CACHE_KEY = 'cache:' + BASE_DASHBOARD_PATH;
-
-export const DASHBOARD_CONSTANTS = {
-	BASE: BASE_DASHBOARD_PATH,
-};
