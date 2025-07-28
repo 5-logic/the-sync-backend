5984462902308367b02a4312bc866859fc1e7002
Quốc Chương <quocchuong2609@gmail.com>
2025-07-28 10:53:47 +0700

    feat: refactor checklist item module to include lecturer and moderator controllers and services; add index file for checklist items

---
+++ src/domains/checklists/checklist-items/index.ts
@@ -0,0 +1,6 @@
+export * from '@/checklists/checklist-items/checklist-item.module';
+export * from '@/checklists/checklist-items/constants';
+export * from '@/checklists/checklist-items/controllers';
+export * from '@/checklists/checklist-items/docs';
+export * from '@/checklists/checklist-items/dtos';
+export * from '@/checklists/checklist-items/services';
