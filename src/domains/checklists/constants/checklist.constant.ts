d4eb0f3b8152b9fe7203d78998927e0ed0929ae2
Quốc Chương <quocchuong2609@gmail.com>
2025-07-28 08:51:02 +0700

    feat: add checklist constants for API integration

---
+++ src/domains/checklists/constants/checklist.constant.ts
@@ -0,0 +1,9 @@
+export const CHECKLIST_API_TAGS = 'Checklist';
+
+export const BASE_CHECKLIST_PATH = 'Checklists';
+
+export const CACHE_KEY = 'cache:' + BASE_CHECKLIST_PATH;
+
+export const CHECKLIST_CONSTANTS = {
+	BASE: BASE_CHECKLIST_PATH,
+};
