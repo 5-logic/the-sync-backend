4dc8cefd225db3b0a35b82e52841959629738d63
Quốc Chương <quocchuong2609@gmail.com>
2025-07-28 15:27:53 +0700

    feat(groups): create index file to export group-related modules and components

--- src/domains/groups/group.module.ts
+++ src/domains/groups/group.module.ts
@@ -1,13 +1,41 @@
 import { Module } from '@nestjs/common';
 
-import { GroupController } from '@/groups/group.controller';
-import { GroupService } from '@/groups/group.service';
+import {
+	GroupLecturerController,
+	GroupModeratorController,
+	GroupPublicController,
+	GroupStudentController,
+} from '@/groups/controllers';
+import {
+	GroupLecturerService,
+	GroupModeratorService,
+	GroupPublicService,
+	GroupStudentService,
+} from '@/groups/services';
+import { GroupService } from '@/groups/services/group.service';
 import { EmailModule } from '@/queue/email/email.module';
 
 @Module({
 	imports: [EmailModule],
-	controllers: [GroupController],
-	providers: [GroupService],
-	exports: [GroupService],
+	controllers: [
+		GroupModeratorController,
+		GroupStudentController,
+		GroupPublicController,
+		GroupLecturerController,
+	],
+	providers: [
+		GroupModeratorService,
+		GroupStudentService,
+		GroupPublicService,
+		GroupLecturerService,
+		GroupService,
+	],
+	exports: [
+		GroupModeratorService,
+		GroupStudentService,
+		GroupPublicService,
+		GroupLecturerService,
+		GroupService,
+	],
 })
 export class GroupModule {}
---
+++ src/domains/groups/index.ts
@@ -0,0 +1,6 @@
+export * from '@/groups/constants';
+export * from '@/groups/controllers';
+export * from '@/groups/docs';
+export * from '@/groups/dtos';
+export * from '@/groups/group.module';
+export * from '@/groups/services';
