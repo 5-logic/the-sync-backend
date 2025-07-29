6b9f3e46298894a56d8b58c6fc2c849c573a17d7
Quốc Chương <quocchuong2609@gmail.com>
2025-07-29 20:32:27 +0700

    feat: add group submission constants for API integration

---
+++ src/domains/groups/constants/group-submission.constant.ts
@@ -0,0 +1,7 @@
+export const GROUP_SUBMISSION_API_TAGS = 'Group Submissions';
+
+export const BASE_GROUP_SUBMISSION_PATH = 'groups';
+
+export const GROUP_SUBMISSION_CONSTANTS = {
+	BASE: BASE_GROUP_SUBMISSION_PATH,
+};
