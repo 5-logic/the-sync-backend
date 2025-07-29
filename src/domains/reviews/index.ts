2956e51edbc45e15ef84cb903b6b819b48a37427
Quốc Chương <quocchuong2609@gmail.com>
2025-07-29 19:25:06 +0700

    feat: create index file for reviews exports

---
+++ src/domains/reviews/index.ts
@@ -0,0 +1,6 @@
+export * from '@/reviews/constants';
+export * from '@/reviews/controllers';
+export * from '@/reviews/docs';
+export * from '@/reviews/dtos';
+export * from '@/reviews/review.module';
+export * from '@/reviews/services';
