6ee8c67bb9895a5a24481c1dce472b17c2e42842
Quốc Chương <quocchuong2609@gmail.com>
2025-07-29 19:18:37 +0700

    refactor: remove unused DTOs related to review management

--- src/domains/reviews/dto/index.ts
+++
@@ -1,6 +0,0 @@
-export * from '@/reviews/dto/assign-bulk-reviewer.dto';
-export * from '@/reviews/dto/create-review-item.dto';
-export * from '@/reviews/dto/create-review.dto';
-export * from '@/reviews/dto/update-review-item.dto';
-export * from '@/reviews/dto/update-review.dto';
-export * from '@/reviews/dto/update-reviewer-assignment.dto';
--- src/domains/reviews/dto/update-review-item.dto.ts
+++
@@ -1,5 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-
-import { CreateReviewItemDto } from '@/reviews/dto';
-
-export class UpdateReviewItemDto extends PartialType(CreateReviewItemDto) {}
