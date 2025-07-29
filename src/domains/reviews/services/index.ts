77499d86c6fb406ed00269d6aac47d9568633474
Quốc Chương <quocchuong2609@gmail.com>
2025-07-29 19:22:49 +0700

    feat: add index file for review services exports

---
+++ src/domains/reviews/services/index.ts
@@ -0,0 +1,4 @@
+export * from '@/reviews/services/review-lecturer.service';
+export * from '@/reviews/services/review-moderator.service';
+export * from '@/reviews/services/review-public.service';
+export * from '@/reviews/services/review.service';
