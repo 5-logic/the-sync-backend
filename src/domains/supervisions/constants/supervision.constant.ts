8fc23ac9f06cf5f8d369643a1e625572ba5bf8bc
Quốc Chương <quocchuong2609@gmail.com>
2025-07-30 08:16:41 +0700

    feat: add supervision constants for API integration

---
+++ src/domains/supervisions/constants/supervision.constant.ts
@@ -0,0 +1,9 @@
+export const SUPERVISION_API_TAGS = 'Supervision';
+
+export const BASE_SUPERVISION_PATH = 'supervisions';
+
+export const CACHE_KEY = 'cache:' + BASE_SUPERVISION_PATH;
+
+export const SUPERVISION_CONSTANTS = {
+	BASE: BASE_SUPERVISION_PATH,
+};
