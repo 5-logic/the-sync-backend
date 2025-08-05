c208e8a6d3165a91a3e2b2425e5f992dcc36075c
Minh Vương <hardingadonis@gmail.com>
2025-08-05 19:07:04 +0700

    refactor: remove unused student and group processors and services from Pinecone module

--- src/queue/pinecone/pinecone.module.ts
+++ src/queue/pinecone/pinecone.module.ts
@@ -6,16 +6,8 @@ import { ConfigModule } from '@nestjs/config';
 
 import { pineconeConfig } from '@/configs';
 import { PINECONE_TOKENS } from '@/queue/pinecone/constants';
-import {
-	PineconeGroupProcessor,
-	PineconeStudentProcessor,
-	PineconeThesisProcessor,
-} from '@/queue/pinecone/processors';
-import {
-	PineconeGroupService,
-	PineconeStudentService,
-	PineconeThesisService,
-} from '@/queue/pinecone/services';
+import { PineconeThesisProcessor } from '@/queue/pinecone/processors';
+import { PineconeThesisService } from '@/queue/pinecone/services';
 
 @Global()
 @Module({
@@ -28,33 +20,8 @@ import {
 			name: PINECONE_TOKENS.THESIS,
 			adapter: BullMQAdapter,
 		}),
-		BullModule.registerQueue({
-			name: PINECONE_TOKENS.STUDENT,
-		}),
-		BullBoardModule.forFeature({
-			name: PINECONE_TOKENS.STUDENT,
-			adapter: BullMQAdapter,
-		}),
-		BullModule.registerQueue({
-			name: PINECONE_TOKENS.GROUP,
-		}),
-		BullBoardModule.forFeature({
-			name: PINECONE_TOKENS.GROUP,
-			adapter: BullMQAdapter,
-		}),
 	],
-	providers: [
-		PineconeThesisProcessor,
-		PineconeThesisService,
-		PineconeStudentProcessor,
-		PineconeStudentService,
-		PineconeGroupProcessor,
-		PineconeGroupService,
-	],
-	exports: [
-		PineconeStudentService,
-		PineconeThesisService,
-		PineconeGroupService,
-	],
+	providers: [PineconeThesisProcessor, PineconeThesisService],
+	exports: [PineconeThesisService],
 })
 export class PineconeModule {}
--- src/queue/pinecone/services/index.ts
+++ src/queue/pinecone/services/index.ts
@@ -1,3 +1,1 @@
 export * from '@/queue/pinecone/services/pinecone-thesis.service';
-export * from '@/queue/pinecone/services/pinecone-student.service';
-export * from '@/queue/pinecone/services/pinecone-group.service';
