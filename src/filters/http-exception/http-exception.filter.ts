5832466432005c2650fadc8debced6fb27ab94ff
Minh Vương <hardingadonis@gmail.com>
2025-08-08 09:00:45 +0700

    feat: add logging for HTTP exceptions in HttpExceptionFilter

--- src/filters/http-exception/http-exception.filter.ts
+++ src/filters/http-exception/http-exception.filter.ts
@@ -4,17 +4,22 @@ import {
 	ExceptionFilter,
 	HttpException,
 	HttpStatus,
+	Logger,
 } from '@nestjs/common';
 import { Response } from 'express';
 
 @Catch()
 export class HttpExceptionFilter implements ExceptionFilter {
+	private readonly logger = new Logger(HttpExceptionFilter.name);
+
 	catch(exception: unknown, host: ArgumentsHost) {
 		const ctx = host.switchToHttp();
 		const res = ctx.getResponse<Response>();
 
 		let status: number;
 		let errorMessage: string | object;
+
+		this.logger.error('An error occurred', exception);
 
 		if (exception instanceof HttpException) {
 			status = exception.getStatus();
