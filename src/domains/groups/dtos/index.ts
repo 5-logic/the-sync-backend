1ab55c92ac6e0a3745f7933e33827fa27abdbcd6
Quốc Chương <quocchuong2609@gmail.com>
2025-08-13 19:51:03 +0700

    feat: add CreateManyGroupDto for batch group creation

--- src/domains/groups/dtos/index.ts
+++ src/domains/groups/dtos/index.ts
@@ -4,3 +4,4 @@ export * from '@/groups/dtos/change-leader.dto';
 export * from '@/groups/dtos/assign-student.dto';
 export * from '@/groups/dtos/remove-student.dto';
 export * from '@/groups/dtos/pick-thesis.dto';
+export * from '@/groups/dtos/create-many-group.dto';
