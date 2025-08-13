e4cb568893bb9de518af71bf57f063a005a1274b
Quốc Chương <quocchuong2609@gmail.com>
2025-08-13 13:50:44 +0700

    feat: add thesis application constants for API integration

---
+++ src/domains/thesis-application/constants/thesis-application.constant.ts
@@ -0,0 +1,7 @@
+export const THESIS_APPLICATION_API_TAGS = 'Thesis Application';
+
+export const BASE_THESIS_APPLICATION_PATH = 'thesis-application';
+
+export const THESIS_APPLICATION_CONSTANTS = {
+	BASE: BASE_THESIS_APPLICATION_PATH,
+};
