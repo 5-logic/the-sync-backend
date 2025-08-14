8ae4d92de0acd6e4dae03a0536f397a47cd95cf0
Quốc Chương <quocchuong2609@gmail.com>
2025-08-14 21:14:16 +0700

    feat: remove max_group column from semesters table and update schema

---
+++ prisma/migrations/20250814141326_/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `max_group` on the `semesters` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "semesters" DROP COLUMN "max_group";
