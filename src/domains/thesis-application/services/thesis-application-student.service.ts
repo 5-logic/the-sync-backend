d3c36080beb850fe9e45ddde89ce6b802b9eb993
Minh Vương <hardingadonis@gmail.com>
2025-08-29 20:20:43 +0700

    fix: update student enrollment status validation to allow 'Ongoing' phase

--- src/domains/thesis-application/services/thesis-application-student.service.ts
+++ src/domains/thesis-application/services/thesis-application-student.service.ts
@@ -44,7 +44,9 @@ export class ThesisApplicationStudentService {
 						enrollments: {
 							where: {
 								semesterId: semesterId,
-								status: 'NotYet',
+								status: {
+									in: ['NotYet', 'Ongoing'],
+								},
 							},
 						},
 					},
